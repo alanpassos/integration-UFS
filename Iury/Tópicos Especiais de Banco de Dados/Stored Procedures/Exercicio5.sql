3345d1e81c5b2f1ed86d6884c973d73caee3ad11
Iury Batista <iurybas@gmail.com>
2015-04-16 17:40:55 -0300

    Atualizacao dos scripts de exercicios de banco e inclusao de estiloDetalhe.css

--- Iury/Tópicos Especiais de Banco de Dados/Stored Procedures/Exercicio5.sql
+++ Iury/Tópicos Especiais de Banco de Dados/Stored Procedures/Exercicio5.sql
@@ -1,11 +1,10 @@
---1.Criar um procedimento para calcular o IMPOSTO DE RENDA a ser pago em  função de 
---uma  determinada  Renda.  O  procedimento  deve  apresentar  como  parâmetro  de  entrada  a 
---Renda,  e  como  parâmetro  de  saída  o  imposto  a  ser  pago.  Considere  as  seguintes
---informações para cálculo do imposto
---:
---Rendimentos do Trabalho
---: 15% e 27,5% conforme tabela progressiva mensal abaixo 
---reproduzida: 
+/*1.Criar um procedimento para calcular o IMPOSTO DE RENDA a ser pago em  função de uma  determinada  Renda.  O  procedimento  deve  
+apresentar  como  parâmetro  de  entrada  a Renda,  e  como  parâmetro  de  saída  o  imposto  a  ser  pago.  Considere  as  seguintes
+informações para cálculo do imposto:
+
+Rendimentos do Trabalho: 15% e 27,5% conforme tabela progressiva mensal abaixo reproduzida: 
+*/
+
 
 ALTER PROCEDURE SP_CALCULAR_IMPOSTO_RENDA (@RENDA NUMERIC(10,2), @PAGAMENTO NUMERIC(10,2) OUTPUT) AS
 IF (@RENDA < 1372.82) 
@@ -14,11 +13,11 @@ BEGIN
 END
 ELSE IF ((@RENDA >= 1372.82) AND (@RENDA <= 2743.25))
 BEGIN
-	SET @PAGAMENTO = @RENDA * 0.15
+	SET @PAGAMENTO = (@RENDA * 0.15) - 205.92
 END
 ELSE
 BEGIN
-	SET @PAGAMENTO = @RENDA * 0.275
+	SET @PAGAMENTO = (@RENDA * 0.275) - 548.82
 END
 
 DECLARE @PAGAMENTO NUMERIC(10,2)
