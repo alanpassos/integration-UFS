ce15e0383d94b679ce68b8cf63f75a909530bdfb
Iury Batista <iurybas@gmail.com>
2015-04-21 15:05:50 -0300

    Criação do Projeto de Robotica

---
+++ Grupos/IA/Alan_Brendel_Geovanne_Iury/RoboAspirador/src/RoboAspirador.java
@@ -0,0 +1,23 @@
+
+public class RoboAspirador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class Estados
+{
+	char nome;
+	boolean salaEsquerda = false;
+	boolean salaDireita = false;
+	
+	public Estados(char nome)
+	{
+		this.nome = nome;
+	}
+	
+	
+}
