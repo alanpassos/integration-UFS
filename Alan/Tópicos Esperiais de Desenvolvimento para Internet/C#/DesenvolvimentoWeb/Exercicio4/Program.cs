2fa98136ade073b88fc00f812d3326b116b1b6d7
Alan Passos <alanpassossi@gmail.com>
2015-04-28 02:52:56 -0300

    Exercicio de Desenvolvimento para internet

---
+++ Alan/Tópicos Esperiais de Desenvolvimento para Internet/C#/DesenvolvimentoWeb/Exercicio4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circulo circulo = new Circulo();
+
+            circulo.InsertData();
+
+            PrintData(circulo);
+
+            Console.ReadKey();
+
+
+
+        }
+
+        private static void PrintData(Circulo circulo)
+        {
+
+            Console.WriteLine("Área: " + circulo.CalcularArea());
+            Console.WriteLine("Comprimento: " + circulo.CalcularComprimento());
+        }
+    }
+}
