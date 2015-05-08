6b8438449e5466fc1df0a9cc312840ad336827ea
alanpassossi <alanpassossi@gmail.com>
2015-05-08 19:58:20 -0300

    chat

---
+++ Edson/ChatCliente/ChatCliente/frmCliente.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ChatCliente
+{
+    partial class frmCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServidorIP = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtServidorIP
+            // 
+            this.txtServidorIP.Font = new System.Drawing.Font("Cambria", 10F);
+            this.txtServidorIP.Location = new System.Drawing.Point(105, 24);
+            this.txtServidorIP.Name = "txtServidorIP";
+            this.txtServidorIP.Size = new System.Drawing.Size(196, 23);
+            this.txtServidorIP.TabIndex = 0;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Cambria", 10F);
+            this.txtUsuario.Location = new System.Drawing.Point(105, 65);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(196, 23);
+            this.txtUsuario.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 10F);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(17, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Servidor IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 10F);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(21, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Usuário";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(24, 106);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(396, 377);
+            this.txtLog.TabIndex = 4;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Font = new System.Drawing.Font("Cambria", 10F);
+            this.btnConectar.Location = new System.Drawing.Point(332, 60);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(87, 27);
+            this.btnConectar.TabIndex = 5;
+            this.btnConectar.Text = "&Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Location = new System.Drawing.Point(24, 496);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(277, 23);
+            this.txtMensagem.TabIndex = 6;
+            this.txtMensagem.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMensagem_KeyPress);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(325, 496);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(98, 27);
+            this.btnEnviar.TabIndex = 7;
+            this.btnEnviar.Text = "&Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Cambria", 10F);
+            this.btnFechar.Location = new System.Drawing.Point(382, 20);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(37, 27);
+            this.btnFechar.TabIndex = 8;
+            this.btnFechar.Text = "X";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // frmCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(446, 533);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtServidorIP);
+            this.Font = new System.Drawing.Font("Cambria", 10F);
+            this.Name = "frmCliente";
+            this.Text = "Chat - Cliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServidorIP;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.TextBox txtMensagem;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
---
+++ Edson/Servidor/ChatServidor/frmServidor.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ChatServidor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnAtender = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Font = new System.Drawing.Font("Cambria", 10F);
+            this.txtLog.Location = new System.Drawing.Point(13, 64);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(438, 229);
+            this.txtLog.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 10F);
+            this.label1.Location = new System.Drawing.Point(13, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Endereço IP : ";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Font = new System.Drawing.Font("Cambria", 10F);
+            this.txtIP.Location = new System.Drawing.Point(110, 29);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(179, 23);
+            this.txtIP.TabIndex = 2;
+            // 
+            // btnAtender
+            // 
+            this.btnAtender.Font = new System.Drawing.Font("Cambria", 10F);
+            this.btnAtender.Location = new System.Drawing.Point(303, 29);
+            this.btnAtender.Name = "btnAtender";
+            this.btnAtender.Size = new System.Drawing.Size(148, 23);
+            this.btnAtender.TabIndex = 3;
+            this.btnAtender.Text = "Iniciar Atendimento";
+            this.btnAtender.UseVisualStyleBackColor = true;
+            this.btnAtender.Click += new System.EventHandler(this.btnAtender_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.NavajoWhite;
+            this.ClientSize = new System.Drawing.Size(463, 305);
+            this.Controls.Add(this.btnAtender);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLog);
+            this.Name = "Form1";
+            this.Text = "Chat Servidor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button btnAtender;
+    }
+}
+
