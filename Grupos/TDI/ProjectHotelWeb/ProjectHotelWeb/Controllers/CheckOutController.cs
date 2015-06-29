c5c2b4ca2f7dbd96a910140ad53af21185886f06
BrendelSantos <brendelsantos@gmail.com>
2015-06-29 17:12:21 -0300

    Check-in, Check-out (views e controller) inseridos

--- Grupos/TDI/ProjectHotelWeb/ProjectHotelWeb/Controllers/CheckOutController.cs
+++ Grupos/TDI/ProjectHotelWeb/ProjectHotelWeb/Controllers/CheckOutController.cs
@@ -6,23 +6,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
+
 namespace ProjectHotelWeb.Controllers
 {
-    public class ReservaController : Controller
+    public class CheckOutController : Controller
     {
         public IProjectHotel IProjectHotel { get; set; }
         public IPacoteHospedagens IPacoteHospedagens { get; set; }
-
-        // GET: Reserva
+        
+        // GET: CheckOut
         public ActionResult Index()
         {
-            List<PacoteHospedagem> pacoteHospedagens = IPacoteHospedagens.ListarReserva().ToList<PacoteHospedagem>();
+            List<PacoteHospedagem> pacoteHospedagens = IPacoteHospedagens.ListarCheckout().ToList<PacoteHospedagem>();
             return View(pacoteHospedagens);
         }
 
         public ActionResult Detalhar(int id)
         {
-            PacoteHospedagem pacoteHospedagem = IPacoteHospedagens.ResultadoUnicoReserva(id);
+            PacoteHospedagem pacoteHospedagem = IPacoteHospedagens.ResultadoUnicoCheckout(id);
             return View(pacoteHospedagem);
         }
 
@@ -31,41 +33,29 @@ namespace ProjectHotelWeb.Controllers
             return View();
         }
 
-        [ActionName("CadastrarReserva")]
+        [ActionName("CadastrarCheckout")]
         public ActionResult Cadastrar(PacoteHospedagem pacoteHospedagem)
         {
             pacoteHospedagem.ativo = true;
-            pacoteHospedagem.tipoPacote = "R";
+            pacoteHospedagem.tipoPacote = "K";
             IPacoteHospedagens.Cadastrar(pacoteHospedagem);
             return RedirectToAction("Index");
         }
 
         public ActionResult Atualizar(int id)
         {
-            PacoteHospedagem pacoteHospedagem = IPacoteHospedagens.ResultadoUnicoReserva(id);
+            PacoteHospedagem pacoteHospedagem = IPacoteHospedagens.ResultadoUnicoCheckout(id);
             return View(pacoteHospedagem);
         }
 
-        [ActionName("AtualizarReserva")]
+        [ActionName("AtualizarCheckout")]
         public ActionResult Atualizar(PacoteHospedagem pacoteHospedagem)
         {
             pacoteHospedagem.ativo = true;
-            pacoteHospedagem.tipoPacote = "R";
+            pacoteHospedagem.tipoPacote = "K";
             IPacoteHospedagens.Atualizar(pacoteHospedagem);
             return RedirectToAction("Index");
         }
 
-        public ActionResult Excluir(int id)
-        {
-            PacoteHospedagem pacoteHospedagem = IPacoteHospedagens.ResultadoUnicoReserva(id);
-            return View(pacoteHospedagem);
-        }
-
-        [ActionName("ExcluirReserva")]
-        public ActionResult Excluir(PacoteHospedagem pacoteHospedagem)
-        {
-            IPacoteHospedagens.Remover(pacoteHospedagem);
-            return RedirectToAction("Index");
-        }
     }
 }
