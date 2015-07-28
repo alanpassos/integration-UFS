b9f9fdb33f06bc07c5d96e346dc81b7e5e07e260
BrendelSantos <brendelsantos@gmail.com>
2015-07-28 11:18:51 -0300

    Controller Item, index de item - falta o resto

---
+++ Grupos/TDI/ProjectHotelWeb/ProjectHotelWeb/Controllers/ItemController.cs
@@ -0,0 +1,24 @@
+﻿using Dominio.Classes;
+using Dominio.Interfaces;
+using Infraestrutura.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectHotelWeb.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IProjectHotel IProjectHotel { get; set; }
+        public IItens IItem { get; set; }
+
+        // GET: Item
+        public ActionResult Index()
+        {
+            List<Item> itens = IItem.Listar().ToList<Item>();
+            return View(itens);
+        }
+    }
+}
