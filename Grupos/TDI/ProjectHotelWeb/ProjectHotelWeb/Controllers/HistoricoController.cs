67d8922732c5d2fc84a5d7715e31312bc1522f07
BrendelSantos <brendelsantos@gmail.com>
2015-07-28 11:03:24 -0300

    Criação da controller Histórico

---
+++ Grupos/TDI/ProjectHotelWeb/ProjectHotelWeb/Controllers/HistoricoController.cs
@@ -0,0 +1,25 @@
+﻿using Dominio.Classes;
+using Dominio.Interfaces;
+using Infraestrutura.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectHotelWeb.Controllers
+{
+    public class HistoricoController : Controller
+    {
+
+        public IProjectHotel IProjectHotel { get; set; }
+        public IHistoricos IHistorico { get; set; }
+
+        // GET: Historico
+        public ActionResult Index()
+        {
+            List<Historico> historicos = IHistorico.Listar().ToList<Historico>();
+            return View(historicos);
+        }
+    }
+}
