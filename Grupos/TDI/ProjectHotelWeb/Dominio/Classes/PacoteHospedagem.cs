118b18431f78c6b41d802633cf8f9caef56a9a54
Geovanne Barreto <geovannesdi@gmail.com>
2015-08-04 12:15:50 -0300

    Commit de quarto atualizado

--- Grupos/TDI/ProjectHotelWeb/Dominio/Classes/PacoteHospedagem.cs
+++ Grupos/TDI/ProjectHotelWeb/Dominio/Classes/PacoteHospedagem.cs
@@ -24,6 +24,7 @@ namespace Dominio.Classes
         public virtual List<Hospedagem> Hospedagem { get; set; }
         public virtual List<Pagamento> Pagamento { get; set; }
         public virtual List<TipoQuarto> TipoQuarto { get; set; }
+        
 
     }
 }
