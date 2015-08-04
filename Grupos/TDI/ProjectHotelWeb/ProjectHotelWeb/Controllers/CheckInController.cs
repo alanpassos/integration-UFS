9158c7e1a75e7483eade033cea174eac33dc1272
alanpassossi <alanpassossi@gmail.com>
2015-08-04 13:02:08 -0300

    Atualização de checkin

--- Grupos/TDI/ProjectHotelWeb/ProjectHotelWeb/Controllers/CheckInController.cs
+++ Grupos/TDI/ProjectHotelWeb/ProjectHotelWeb/Controllers/CheckInController.cs
@@ -70,17 +70,24 @@ namespace ProjectHotelWeb.Controllers
 
         public ActionResult AtualizarCliente()
         {
-            string cpfMask = Request.Params.Get("Consulta");
-            adicionarPessoas(cpfMask);
+            string consulta = Request.Params.Get("Consulta");
+            string filtro = Request.Params.Get("Filtro");
+            adicionarPessoas(consulta, filtro);
             ViewBag.Pessoas = pessoasAdicionadas;
             return View("Checkin");
         }
 
-        private void adicionarPessoas(string cpfMask)
-        {
-            string cpf = cpfMask.Replace(".", "").Replace("-", "");
 
-            List<Pessoa> pessoas = IPessoas.ListarPorCpfCnpj(cpf).ToList<Pessoa>();
+
+
+        /// <summary>
+        /// Adiciona as pessoas que estão cadastrardas no sistema
+        /// </summary>
+        /// <param name="consulta">Valor no text para a consulta</param>
+        /// <param name="filtro">Combo relacionado ao tipo de dados para a consulta</param>
+        private void adicionarPessoas(string consulta, string filtro)
+        {
+            List<Pessoa> pessoas = FiltroConsulta(consulta, filtro);
 
             if (ViewBag.Pessoas != null)
             {
@@ -92,9 +99,38 @@ namespace ProjectHotelWeb.Controllers
                 pessoasAdicionadas.Add(pessoa);
 
             }
+        }
+        /// <summary>
+        /// Filtro da consulta para o retorno das pessoas selecionadas
+        /// </summary>
+        /// <param name="consulta">Valores para o campo de consulta</param>
+        /// <param name="filtro">Cpf, Cnpj, Telefone ou Nome</param>
+        /// <returns></returns>
+        private List<Pessoa> FiltroConsulta(string consulta, string filtro)
+        {
+            List<Pessoa> pessoas = new List<Pessoa>();
+            switch (Convert.ToInt32(filtro))
+            {
 
+                case 1:
+                    string cpf = consulta.Replace(".", "").Replace("-", "");
+                    pessoas = IPessoas.ListarPorCpfCnpj(cpf).ToList<Pessoa>();
+                    break;
+                case 2:
+                    string cnpj = consulta.Replace(".", "").Replace("-", "").Replace("/", "");
+                    pessoas = IPessoas.ListarPorCpfCnpj(cnpj).ToList<Pessoa>();
+                    break;
+                case 3:
+                    string nome = consulta;
+                    pessoas = IPessoas.ListarPorNome(nome).ToList<Pessoa>();
+                    break;
+                case 4:
+                    string telefone = consulta.Replace("(", "").Replace(")", "").Replace("-", "");
+                    pessoas = IPessoas.ListarPorNome(telefone).ToList<Pessoa>();
+                    break;
 
-
+            }
+            return pessoas;
         }
     }
 }
