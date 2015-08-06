61982c99ceb3abec677d2cdb6b53109e34bc89eb
Iury Batista <iurybas@gmail.com>
2015-08-06 11:35:19 -0300

    Inclusão dos métodos de Usuário basicos

--- Grupos/TDI/ProjectHotelWeb/ProjectHotelWeb/Global.asax.cs
+++ Grupos/TDI/ProjectHotelWeb/ProjectHotelWeb/Global.asax.cs
@@ -1,24 +1,52 @@
-﻿using ExemploWEB.App_Start;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using IdentitySample.Models;
+using System.Data.Entity;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
-using ProjectHotelWeb.ClassesEspeciais;
-using Dominio.Classes;
+using ProjectHotelWeb.Models;
+using ExemploWEB.App_Start;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace ProjectHotelWeb
+namespace IdentitySample
 {
+    // Note: For instructions on enabling IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=301868
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
-            Entidade.hospedagemGlobal = null;
-            SuperClasses.pessoasAdicionadas= new List<Pessoa>();
             AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            ControllerBuilder.Current.SetControllerFactory(new AppConfig());
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+             ControllerBuilder.Current.SetControllerFactory(new AppConfig());
+
+            UserDbContext db = new UserDbContext();
+            RoleStore<UserRole> roleStore = new RoleStore<UserRole>(db);
+            RoleManager<UserRole> roleManager = new RoleManager<UserRole>(roleStore);
+
+           
+            if (!roleManager.RoleExists("Administrador"))
+            {
+                UserRole newRole = new UserRole("Administrador", "Nível máximo de acesso");
+                roleManager.Create(newRole);
+            }
+            else if (!roleManager.RoleExists("Gerente"))
+            {
+                UserRole newRole = new UserRole("Gerente", "Nível elevado de acesso");
+            }
+            else if (!roleManager.RoleExists("Recepcionista"))
+            {
+                UserRole newRole = new UserRole("Recepcionista", "Nível médio de acesso");
+                roleManager.Create(newRole);
+            }
+            else if (!roleManager.RoleExists("Convidado"))
+            {
+                UserRole newRole = new UserRole("Convidado", "Nível básico de acesso");
+                roleManager.Create(newRole);
+            }
         }
     }
 }
