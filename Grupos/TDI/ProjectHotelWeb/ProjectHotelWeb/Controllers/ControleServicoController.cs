3935e2102404ac5517f1fb2bf54e3a7dd78c53fe
Iury Batista <iurybas@gmail.com>
2015-08-10 01:54:17 -0300

    Adição do action vincualr serviço

--- Grupos/TDI/ProjectHotelWeb/ProjectHotelWeb/Controllers/ControleServicoController.cs
+++ Grupos/TDI/ProjectHotelWeb/ProjectHotelWeb/Controllers/ControleServicoController.cs
@@ -9,16 +9,19 @@ using Dominio.Interfaces;
 using Infraestrutura.Database;
 using ProjectHotelWeb.ClassesEspeciais;
 
+
 namespace ProjectHotelWeb.Controllers
 {
     public class ControleServicoController : Controller
     {
         public IProjectHotel IProjectHotel { get; set; }
+        public IControleServicos IControleServicos { get; set; }
         public IPessoas IPessoas { get; set; }
         public IControleClientes ICliente { get; set; }
         public IHospedagens IHospedagens { get; set; }
         public IQuartos IQuartos { get; set; }
         public IServicos IServicos { get; set; }
+        //public UserManager<User> userManager; 
         public List<Servico> listaDeServicos = new List<Servico>();
 
 
@@ -42,6 +45,10 @@ namespace ProjectHotelWeb.Controllers
             pessoaCliente = IPessoas.ResultadoUnico(cliente.Pessoa.idPessoa);
             quarto = IQuartos.ResultadoUnico(hospedagem.idQuarto);
 
+
+            TempData["hospedagem"] = hospedagem;
+
+
             ViewBag.Hospedagem = hospedagem;
             ViewBag.Cliente = cliente;
             ViewBag.Servicos = servicos;
@@ -64,7 +71,32 @@ namespace ProjectHotelWeb.Controllers
             return PartialView(servico);
 
         }
+        
+        public void VincularServico (string idServico)
+        {
+            
+            Hospedagem hospedagem = (Hospedagem) TempData["hospedagem"];
+            
 
+            int codigoServico = Convert.ToInt32(idServico.Trim());
+            double valorTotal = Convert.ToDouble(Request.Params.Get("total").Remove(0, 2));
+            DateTime dataInicio = Convert.ToDateTime(Request.Params.Get("dataInicio"));
+            DateTime dataFim = Convert.ToDateTime(Request.Params.Get("dataFim"));
+            ControleServico controleServico = new ControleServico();
+            controleServico.idHospedagem = hospedagem.idHospedagem;
+            controleServico.idServico = codigoServico;
+            controleServico.idFuncionario = 2;
+            controleServico.dataAbertura = dataInicio;
+            controleServico.dataLiberacao = dataFim;
+            controleServico.dataCadastro = DateTime.Now;
+            controleServico.cancelado = false;
 
+            IControleServicos.Cadastrar(controleServico);
+
+           // return null;
+
+        }
+
+        
     }
 }
