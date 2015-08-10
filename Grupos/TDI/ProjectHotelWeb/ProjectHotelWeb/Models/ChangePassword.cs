f70617ceda8956f8a45e4d6137629791bc308ecb
Iury Batista <iurybas@gmail.com>
2015-08-10 10:18:11 -0300

    Adição de classes do Identity para merge com master

---
+++ Grupos/TDI/ProjectHotelWeb/ProjectHotelWeb/Models/ChangePassword.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjectHotelWeb.Models
+{
+    public class ChangePassword
+    {
+
+        [Required]
+        public string OldPassword { get; set; }
+
+        [Required]
+        public string NewPassword { get; set; }
+        
+        [Required]
+        [Compare("NewPassword", ErrorMessage="As senhas não conferem")]
+        public string ConfirmPassword { get; set; }
+    }
+}
