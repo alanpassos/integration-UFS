56afd50c800b9117c0be2e2d7cebc225b74be7b1
BrendelSantos <brendelsantos@gmail.com>
2015-08-11 04:31:58 -0300

    Remoção arquivos

---
+++ Grupos/TDI/ProjectHotelWeb/Dominio/Classes Especiais/HospedagemMes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Classes_Especiais
+{
+    public class HospedagemMes
+    {
+        public string nome { get; set; }
+        public string cpfcnpj { get; set; }
+        public string telefonemovel { get; set; }
+        public int hospedagens { get; set; }
+        public string mes { get; set; }
+        public decimal valorTotal { get; set; }
+
+    }
+}
--- Grupos/TDI/ProjectHotelWeb/Infraestrutura/Repositorio/Hospedagens.cs
+++ Grupos/TDI/ProjectHotelWeb/Infraestrutura/Repositorio/Hospedagens.cs
@@ -1,11 +1,14 @@
 ﻿using Dominio.Classes;
 using Dominio.Interfaces;
 using Infraestrutura.Database;
+using Dominio.Classes_Especiais;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
+using System.Data.Entity;
 
 namespace Infraestrutura.Repositorio
 {
@@ -13,11 +16,24 @@ namespace Infraestrutura.Repositorio
     {
         private IQueryable<Hospedagem> hospedagens;
         private IProjectHotel unidadeTrabalho;
+        private IQueryable<Quarto> quartos;
+        private IQueryable<TipoQuarto> tipoQuartos;
+        private IQueryable<ControleCliente> controleClientes;
+        private IQueryable<Pessoa> clientes;
+        private IQueryable<PacoteHospedagem> pacoteHospedagens;
+
+
 
         private Hospedagens(IQueryable<Hospedagem> hospedagens, IProjectHotel unidadeTrabalho)
         {
             this.hospedagens = hospedagens;
             this.unidadeTrabalho = unidadeTrabalho;
+            tipoQuartos = unidadeTrabalho.TipoQuartos;
+            quartos = unidadeTrabalho.Quartos;
+            hospedagens = unidadeTrabalho.Hospedagens;
+            controleClientes = unidadeTrabalho.ControleClientes;
+            clientes = unidadeTrabalho.Clientes;
+            pacoteHospedagens = unidadeTrabalho.PacoteHospedagens;
         }
 
         public Hospedagens(IProjectHotel iHotelWeb, IProjectHotel unidadeTrabalho) :
@@ -51,6 +67,51 @@ namespace Infraestrutura.Repositorio
         {
             return hospedagens.OrderBy(p => p.dataAbertura).ToList();
         }
+
+        public ICollection<HospedagemMes> RelatorioHospedagemMes()
+        {
+            List<HospedagemMes> listaHospedagem = new List<HospedagemMes>();
+            CultureInfo culture = new CultureInfo("pt-BR");
+            IQueryable<HospedagemMes> ConsultaHospedagemMes = from hospedagem in hospedagens
+                                                              join controleCliente in controleClientes on hospedagem.idHospedagem equals controleCliente.idHospedagem
+                                                              join pessoa in clientes on controleCliente.idCliente equals pessoa.idPessoa
+                                                              join pacoteHospedagem in pacoteHospedagens on hospedagem.idPacoteHospedagem equals pacoteHospedagem.idPacoteHospedagem
+                                                              where hospedagem.dataAbertura.Month <= DateTime.Now.Month
+                                                                && pacoteHospedagem.tipoPacote.Equals("C")
+                                                                group hospedagem by new
+                                                                {
+                                                                    pessoa.nome,
+                                                                    hospedagem.dataAbertura,
+                                                                    pessoa.cpfCnpj,
+                                                                    hospedagem.valorHospedagem,
+                                                                    pessoa.telefoneMovel,
+                                                                    mes = hospedagem.dataAbertura.Month
+                                                                    
+                                                                } into hospedagemMes
+                                                              select new HospedagemMes
+                                                              {
+                                                                  nome = hospedagemMes.Key.nome,
+                                                                  cpfcnpj = hospedagemMes.Key.cpfCnpj,
+                                                                  telefonemovel = hospedagemMes.Key.telefoneMovel,
+                                                                  hospedagens = hospedagemMes.Count(),
+                                                                  mes = (hospedagemMes.Key.mes).ToString(),
+                                                                  valorTotal = hospedagemMes.Sum(p => p.valorHospedagem)
+                                                              };
+
+            var _listaHospedagem = ConsultaHospedagemMes.ToList().Select(c => new HospedagemMes
+                {
+                    nome = c.nome,
+                    cpfcnpj = c.cpfcnpj,
+                    telefonemovel = c.telefonemovel,
+                    hospedagens = c.hospedagens,
+                    mes = culture.DateTimeFormat.GetMonthName(Convert.ToInt32(c.mes)).ToString().ToUpper(),
+                    valorTotal = c.valorTotal
+                });
+            listaHospedagem = _listaHospedagem.ToList();
+
+            return listaHospedagem;
+        }
+
 
         public bool ContemRegistro()
         {
