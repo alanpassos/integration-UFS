c2ef06a79367a10d4fb23d46f33ee4eeb84af460
Iury Batista <iurybas@gmail.com>
2015-09-03 21:17:29 -0300

    Adição da classe de test de Checkout

---
+++ Grupos/TDI/ProjectHotelWeb/ProjectHotelWebTest/CheckoutTeste.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Infraestrutura.Repositorio;
+using Dominio.Classes;
+
+namespace ProjectHotelWebTest
+{
+    [TestClass]
+    public class CheckoutTeste
+    {
+
+        private TestContext testContextInstance;
+        private PacoteHospedagem pacoteHospedagem { get; set; }
+
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        [TestInitialize]
+        public void ObtemPacoteHospedagem()
+        {
+            PacoteHospedagens gPacoteHospedagem = new PacoteHospedagens();
+            PacoteHospedagem pacoteHospedam = gPacoteHospedagem.ResultadoUnico(1);
+            this.pacoteHospedagem = pacoteHospedagem;
+        }
+
+        [TestMethod]
+        public void Checkout()
+        {
+            
+        }
+    }
+}
